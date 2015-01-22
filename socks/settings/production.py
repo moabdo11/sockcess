479e659dd8ed4f71e72cbc462bccd6208eb1200b
Taliesin Oppenheimer <taliesinb.ops@gmail.com>
2015-01-21 23:14:21 -0500

    changed debug to on

--- socks/settings/production.py
+++ socks/settings/production.py
@@ -5,8 +5,8 @@ from django.conf import settings
 
 SECRET_KEY = '^aaz(sb^g_n%)!x46z9e)o&ovq*ky035m1(%$nr)5=g+jo#tx3'
 
-DEBUG = False
-TEMPLATE_DEBUG = False
+DEBUG = True
+TEMPLATE_DEBUG = True
 
 DATABASES = settings.DATABASES
 
