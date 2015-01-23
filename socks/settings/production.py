46806ffc21abc821bab1f8545cd89e637199adbd
Taliesin Oppenheimer <taliesinb.ops@gmail.com>
2015-01-23 18:37:41 -0500

    changed settings

--- socks/settings/production.py
+++ socks/settings/production.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 SECRET_KEY = '^aaz(sb^g_n%)!x46z9e)o&ovq*ky035m1(%$nr)5=g+jo#tx3'
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = False
 
 DATABASES = settings.DATABASES
@@ -22,7 +22,7 @@ ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
 import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
