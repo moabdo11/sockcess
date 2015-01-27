666bb3dffae3c537bf6e026871d31d9c73acb862
Taliesin Oppenheimer <taliesinb.ops@gmail.com>
2015-01-26 20:17:50 -0500

    added fb shit

--- site_visits/models.py
+++ site_visits/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Visit(models.Model):
+	ip       = models.IPAddressField()
+	time_start     = models.DateTimeField(auto_now_add=True, auto_now=False)
+	time_end
--- site_visits/views.py
+++ site_visits/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.conf import settings
 # Create your views here.
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[-1].strip()
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
+
+
+def session_variables(*args, **kwargs):
+
+	ip 
